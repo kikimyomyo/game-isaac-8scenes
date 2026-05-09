17575bf51114499dc994998f31da09a628e4586e
kikimyomyo <request1004@gmail.com>
2026-05-09 13:31:46 +0900

    deploy: isaac-8scenes

---
+++ config.js
@@ -0,0 +1,6 @@
+window.GAME_CONFIG = {
+  game: "isaac-8scenes",
+  title: "이삭 8장면",
+  sheet_tab: "isaac-8scenes",
+  endpoint: "https://script.google.com/macros/s/AKfycbx3a96h-vRywYTCxOJxdn199uaFAELyfzy7LRIGeInFuG-ZhxsBh_AKso--Yw8PbYPH/exec",
+};
